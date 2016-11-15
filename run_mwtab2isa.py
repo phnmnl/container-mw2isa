a261e524256a41969d813b09e2c4b0b4e60fb388
Philippe Rocca-Serra <philippe.rocca-serra@oerc.ox.ac.uk>
2016-11-15 12:05:53 +0000

    updated docker container, failure caused by pb with isa-api

--- run_mwtab2isa.py
+++ run_mwtab2isa.py
@@ -3,7 +3,8 @@ import os
 mwtab_study_id = sys.argv[1]
 target_dir = sys.argv[2]
 try:
-    from isatools.convert import mw2isatab
+    from isatools.convert import mw2isa
+    mw2isa(mwtab_study_id, target_dir)
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
-mw2isatab.convert(open(mwtab_study_id), target_dir)
+
