08c77a822a71c40e96037d7fe0f2b12c2d4aefab
Philippe Rocca-Serra <philippe.rocca-serra@oerc.ox.ac.uk>
2016-12-08 14:38:02 +0000

    updating testing scripts to latest version of API

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,12 @@ RUN apt-get -y update
 RUN apt-get -y install --no-install-recommends python3-pip
 RUN pip3 install --upgrade pip
 RUN pip3 install -U setuptools
-RUN pip3 install isatools==0.3.4
+RUN pip3 install isatools==0.4.0
 
 # RUN  pip3 uninstall -y pip3
-# RUN    apt-get purge -y python3-pip 
-# RUN    apt-get install --no-install-recommends python && \
-# RUN    apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# RUN  apt-get purge -y python3-pip 
+# RUN  apt-get install --no-install-recommends python && \
+# RUN  apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ADD run_test1.sh /usr/local/bin/run_test1.sh
 
--- run_mwtab2isa.py
+++ run_mwtab2isa.py
@@ -1,10 +1,28 @@
 import sys
 import os
-mwtab_study_id = sys.argv[1]
-target_dir = sys.argv[2]
+
+# mwtab_study_id = sys.argv[1]
+# target_dir = sys.argv[2]
+
 try:
     from isatools.convert import mw2isa
     mw2isa(mwtab_study_id, target_dir)
+	from isatools import isatab
+	from mw2isa import mw2isa_convert
+	
+	success, study_id, validate = mw2isa_convert(studyid="ST000367", outputdir='output', dl_option="no", validate_option="no")
+
+	if success:
+		print("conversion successful")
+	elif success and validate:
+	    print("conversion successful, invoking the validator for " + study_id)
+	    try:
+	        isatab.validate2(open(outputdir + "/" + study_id + '/i_investigation.txt'), './isaconfig-default_v2015-07-02/')
+	    except:
+	        print("conversion successful but validation failed")
+	else        
+else:
+    print("conversion failed, validation was not invoked")
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
 
