85b24d87a8431221bf483cabbb4df101c764bd97
Pablo Moreno <pablo.a.moreno@gmail.com>
2016-12-13 11:26:13 +0000

    Adds graceful exit when no parameters are given.

--- run_mwtab2isa.py
+++ run_mwtab2isa.py
@@ -2,6 +2,11 @@
 
 import sys
 import os
+
+if len(sys.argv) < 3:
+   print("Usage: run_mwtab2isa.py mwtab_study_id target_dir")
+   sys.exit(0)
+
 mwtab_study_id = sys.argv[1]
 target_dir = sys.argv[2]
 try:
