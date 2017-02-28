50149b159f4e341a9804de24644707352ab2134d
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-02-28 15:13:47 +0000

    Merge branch 'develop'

--- Dockerfile
+++ Dockerfile
@@ -7,18 +7,6 @@ LABEL software="mw2isa"
 LABEL software.version="0.5.0"
 LABEL version="0.1"
 
-<<<<<<< HEAD
-RUN apt-get -y update
-RUN apt-get -y install --no-install-recommends python3-pip
-RUN pip3 install --upgrade pip
-RUN pip3 install -U setuptools
-RUN pip3 install isatools==0.4.0
-
-# RUN  pip3 uninstall -y pip3
-# RUN  apt-get purge -y python3-pip 
-# RUN  apt-get install --no-install-recommends python && \
-# RUN  apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-=======
 
 RUN apt-get -y update && apt-get -y install --no-install-recommends python3-pip libxml2-dev libxslt-dev gcc python3-lxml && \
     pip3 install --upgrade pip && \
@@ -27,7 +15,6 @@ RUN apt-get -y update && apt-get -y install --no-install-recommends python3-pip 
     apt-get purge -y python3-pip gcc libxml2-dev libxslt-dev python3-lxml && \
     apt-get install --no-install-recommends python3 && \
     apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
->>>>>>> develop
 
 ADD run_test1.sh /usr/local/bin/run_test1.sh
 RUN chmod a+x /usr/local/bin/run_test1.sh
--- run_mwtab2isa.py
+++ run_mwtab2isa.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-<<<<<<< HEAD
-import os
-
-# mwtab_study_id = sys.argv[1]
-# target_dir = sys.argv[2]
-
-try:
-    from isatools.convert import mw2isa
-    mw2isa(mwtab_study_id, target_dir)
-	from isatools import isatab
-	from mw2isa import mw2isa_convert
-	
-	success, study_id, validate = mw2isa_convert(studyid="ST000367", outputdir='output', dl_option="no", validate_option="no")
-
-	if success:
-		print("conversion successful")
-	elif success and validate:
-	    print("conversion successful, invoking the validator for " + study_id)
-	    try:
-	        isatab.validate2(open(outputdir + "/" + study_id + '/i_investigation.txt'), './isaconfig-default_v2015-07-02/')
-	    except:
-	        print("conversion successful but validation failed")
-	else        
-else:
-    print("conversion failed, validation was not invoked")
-=======
 
 if len(sys.argv) < 3:
    print("Usage: run_mwtab2isa.py mwtab_study_id target_dir")
@@ -37,7 +11,5 @@ target_dir = sys.argv[2]
 try:
     from isatools.convert.mw2isa import mw2isa_convert
     mw2isa_convert(studyid=mwtab_study_id, outputdir=target_dir)
->>>>>>> develop
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
-
