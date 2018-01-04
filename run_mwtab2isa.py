c6b7d95f8ae255e6ff3cf67307b9aff570527782
David Johnson <djcomlab@gmail.com>
2018-01-04 16:11:39 +0000

    Fix for updated package locations in isatools

--- run_mwtab2isa.py
+++ run_mwtab2isa.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 3:
 mwtab_study_id = sys.argv[1]
 target_dir = sys.argv[2]
 try:
-    from isatools.convert.mw2isa import mw2isa_convert
+    from isatools.net.mw2isa import mw2isa_convert
     mw2isa_convert(studyid=mwtab_study_id, outputdir=target_dir)
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
